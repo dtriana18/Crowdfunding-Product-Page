b5c08d80aaaa162a76cd50f7ecb790ab734b5529
dtriana <dstm1801@gmail.com>
2023-02-07 21:24:13 -0500

    Completed full menu functionality for mobile, now i have to do the same with desktop and repair some bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,22 @@
 const hamburguerMenuIcon = document.querySelector('#hamburguerMenuIcon');
 const closeMenuIcon = document.querySelector('#closeMenuIcon');
 const menuDarkOverlayer = document.querySelector("#menuDarkOverlayer");
-const mobileMenu = document.querySelector("mobileMenu");
+const mobileMenu = document.querySelector("#mobileMenu");
 
 function toggleMobileMenu() {
     // ICONS
     hamburguerMenuIcon.classList.toggle("activeIcon");
     closeMenuIcon.classList.toggle("activeIcon");
+
+    // OVERLAYER
     menuDarkOverlayer.classList.toggle("activeMenuOverlayer");
+
+    // ACTUAL MENU
+    mobileMenu.classList.toggle("activeNavbar");
 }
+
+// On overlayer click close menu
+menuDarkOverlayer.addEventListener("click", toggleMobileMenu);
 
 [hamburguerMenuIcon, closeMenuIcon].forEach(menuIcon => {
     menuIcon.addEventListener("click", toggleMobileMenu);

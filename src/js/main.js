b9ab5492e5bda593a473046647739caa3223ef79
dtriana <dstm1801@gmail.com>
2023-02-07 19:07:48 -0500

    Added mobile menu funtionality (JS)

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-// alert("Hola")
+const hamburguerMenuIcon = document.querySelector('#hamburguerMenuIcon');
+const closeMenuIcon = document.querySelector('#closeMenuIcon');
+const menuDarkOverlayer = document.querySelector("#menuDarkOverlayer");
+const mobileMenu = document.querySelector("mobileMenu");
+
+function toggleMobileMenu() {
+    console.log("click");
+    hamburguerMenuIcon.classList.toggle("hidden");
+    hamburguerMenuIcon.classList.toggle("show");
+
+    closeMenuIcon.classList.toggle("show");
+    closeMenuIcon.classList.toggle("hidden");
+}
+
+[hamburguerMenuIcon, closeMenuIcon].forEach(menuIcon => {
+    menuIcon.addEventListener("click", toggleMobileMenu);
+})

f9c3ce25a961dc8a17d65f25dc7987b2fe1b7180
dtriana <dstm1801@gmail.com>
2023-02-21 14:09:39 -0500

    Added better documentation to getElementsFromIds utils function

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,6 +1,7 @@
 /**
- * @param  {...string} ids - Elements ids 
- * @returns {HTMLElement[]}
+ * Returns an array of HTMLElement objects based on their id attributes.
+ * @param {...string} ids - The id attributes of the HTML elements to select.
+ * @returns {HTMLElement[]} An array of HTMLElement objects, in the order they were requested by id.
 */
 export function getElementsFromIds(...ids) {
     return ids.map(id => document.querySelector("#" + id));

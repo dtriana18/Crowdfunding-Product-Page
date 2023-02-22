026e2ab13ebc5a0102d4a0d6e5f4e95fc60a49ea
dtriana <dstm1801@gmail.com>
2023-02-22 10:00:27 -0500

    Added basic global state file

---
+++ src/js/globalState.js
@@ -0,0 +1,11 @@
+const globalState = {
+    totalMoney: 10000,
+    totalBackers: 99,
+    
+    supportNoReward: false,
+    unitsLeft: {
+        bamboo: 0,
+        black: 0,
+        mahogany: 0,
+    },
+}
